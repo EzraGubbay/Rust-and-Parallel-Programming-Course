b660383e941fe98ce9e077c78074f24a69e362c2
Ezra Gubbay <ezragubbay99@gmail.com>
2026-01-31 19:38:53 +0200

    Completed Assignment 2.8: Heterogeneous Serialization Framework - Dynamic Dispatch

---
+++ Assignments/Section2/assignment2_8/src/main.rs
@@ -0,0 +1,42 @@
+// Assignment 2.8: Heterogeneous Serialization Framework
+
+trait Serialize {
+    fn to_json(&self) -> String;
+}
+
+impl Serialize for i32 {
+    fn to_json(&self) -> String {
+        format!("\"{}\"", self)
+    }
+}
+
+impl Serialize for String {
+    fn to_json(&self) -> String {
+        format!("\"{}\"", self)
+    }
+}
+
+struct User {
+    name: String,
+    age: u8,
+}
+
+impl Serialize for User {
+    fn to_json(&self) -> String {
+        return format!("{{ \"name\": \"{}\", \"age\": \"{}\", }}", self.name, self.age);
+    }
+}
+
+fn main() {
+    let mut list: Vec<Box<dyn Serialize>> = Vec::new();
+    list.push(Box::new(10));
+    list.push(Box::new(String::from("Hello")));
+    list.push(Box::new(User {
+        name: String::from("Ezra"),
+        age: 26,
+    }));
+
+    for item in list {
+        println!("{}", item.to_json());
+    }
+}
