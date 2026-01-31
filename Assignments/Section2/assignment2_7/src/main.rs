1f81df6575e890f3d2db257cd32e83ea67d517d6
Ezra Gubbay <ezragubbay99@gmail.com>
2026-01-31 19:14:21 +0200

    Completed Assignment 2.7: Graph Algorithm Framework - Dynamic Traits and Polymorphism in Rust

---
+++ Assignments/Section2/assignment2_7/src/main.rs
@@ -0,0 +1,40 @@
+// Assignment 2.7: Graph Algorithm Framework
+
+trait Graph {
+    type Node;
+    type Edge;
+    fn init(n: Self::Node, e: Self::Edge) -> Self;
+    fn describe(&self);
+}
+
+struct CityGraph {
+    root_city: String,
+    distance: u32,
+}
+
+impl Graph for CityGraph {
+    type Node = String;
+    type Edge = u32;
+
+    fn init(n: Self::Node, e: Self::Edge) -> Self {
+        Self {
+            root_city: n,
+            distance: e,
+        }
+    }
+
+    fn describe(&self) {
+        println!(
+            "City Graph: {} with distance {}",
+            self.root_city, self.distance
+        )
+    }
+}
+
+fn build_and_describe<G: Graph>(n: G::Node, e: G::Edge) {
+    G::init(n, e).describe();
+}
+
+fn main() {
+    build_and_describe::<CityGraph>("London".to_string(), 500);
+}
